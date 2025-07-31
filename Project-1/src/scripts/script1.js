fa8992b76df4fba36d4da671185a03d9a172fcfb
Abhishek Barik <abhishekbarik974@gmail.com>
2025-07-31 20:35:37 +0530

    usrdashboard page add task functionality added

--- Project-1/src/scripts/script1.js
+++
@@ -1,9 +0,0 @@
-const sidebartoggle = document.getElementById('sidebarToggle');
- if(sidebartoggle){
-    sidebartoggle.addEventListener('click',()=>{
-        const sidebar = document.getElementById('sidebar');
-        if(sidebar){
-            sidebar.classList.toggle('hidden');
-        }
-    })
- }
